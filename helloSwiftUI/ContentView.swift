3cc9f483ad6e76a3ec471981508d6fd3484be547
김지하 <kimjiha1112@gmail.com>
2024-07-29 16:17:15 +0900

    Displaying Images

--- helloSwiftUI/ContentView.swift
+++ helloSwiftUI/ContentView.swift
@@ -10,8 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Image("costa-rica")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 25.0, style: .continuous))
+            AsyncImage(url: URL(string:"https://plus.unsplash.com/premium_photo-1721651435483-4e0cabf4d186?q=80&w=2054&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")!) {
+                image in image.resizable()
+            } placeholder: {
+                Text("Downloading...")
+            }
             Text("First Line")
                 .foregroundStyle(.cyan)
+                .font(.title3)
+                .padding([.top, .bottom], 20)
             Text("Second Line")
                 .foregroundStyle(.green)
             Text("Third Line")
