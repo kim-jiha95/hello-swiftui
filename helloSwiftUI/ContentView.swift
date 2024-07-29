25f7e8c680b596ab35658c4e7636fe5d598dc65b
김지하 <kimjiha1112@gmail.com>
2024-07-29 17:32:02 +0900

    add hike contents

--- helloSwiftUI/ContentView.swift
+++ helloSwiftUI/ContentView.swift
@@ -7,32 +7,36 @@
 
 import SwiftUI
 
+struct Hike: Identifiable {
+    let id = UUID()
+    let name: String
+    let photo: String
+    let miles: Double
+}
+
 struct ContentView: View {
+    
+    let hikes = [
+        Hike(name: "Salmonberry Trail", photo: "sal", miles: 6),
+        Hike(name: "Tom, Dick, and Harry Mountain", photo: "tom", miles: 5.8),
+        Hike(name: "Tamanawas Falls", photo: "tam", miles: 5)
+    ]
+    
     var body: some View {
-        VStack {
-            Image("costa-rica")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .clipShape(RoundedRectangle(cornerRadius: 25.0, style: .continuous))
-            AsyncImage(url: URL(string:"https://plus.unsplash.com/premium_photo-1721651435483-4e0cabf4d186?q=80&w=2054&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")!) {
-                image in image.resizable()
-            } placeholder: {
-                Text("Downloading...")
+        List(hikes) { hike in
+            HStack(alignment: .top) {
+                Image(hike.photo)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(RoundedRectangle(cornerRadius: 10.0, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+                    .frame(width: 100)
+                   
+                VStack(alignment: .leading) {
+                    Text(hike.name)
+                    Text("\(hike.miles.formatted()) miles")
+                }
             }
-            Text("First Line")
-                .foregroundStyle(.cyan)
-                .font(.title3)
-                .padding([.top, .bottom], 20)
-            Text("Second Line")
-                .foregroundStyle(.green)
-            Text("Third Line")
-            HStack {
-                Text("Left")
-                Text("Right")
-                    .fontWeight(.heavy)
-            }
-        }.foregroundStyle(.orange)
-        .padding()
+        }
     }
 }
 
