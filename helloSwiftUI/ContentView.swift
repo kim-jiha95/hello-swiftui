532191859b4a5f7ac338dbfdd3060bdc62daa86f
김지하 <kimjiha1112@gmail.com>
2024-07-30 16:35:40 +0900

    filtering list using search

--- helloSwiftUI/ContentView.swift
+++ helloSwiftUI/ContentView.swift
@@ -8,26 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+     
+    @State private var search: String = ""
+    @State private var friends: [String] = ["John", "Mary", "Steven", "Steve", "Jerry"]
     
-    @State private var name: String = ""
-    @State private var friends: [String] = []
+    @State private var filteredFriends: [String] = []
     
     var body: some View {
         VStack {
-            TextField("Enter name", text: $name)
-                .textFieldStyle(.roundedBorder)
-                .onSubmit {
-                    friends.append(name)
-                    name = ""
-                }
-            List(friends, id: \.self) { friend in
+            List(filteredFriends, id: \.self) { friend in
                 Text(friend)
             }
+            .listStyle(.plain)
+            .searchable(text: $search)
+            .onChange(of: search) {
+                if search.isEmpty {
+                    filteredFriends = friends
+                } else {
+                    filteredFriends = friends.filter { $0.contains(search) }
+                }
+            }
+            
             Spacer()
         }.padding()
+        .onAppear(perform: {
+            filteredFriends = friends
+        })
+        .navigationTitle("Friends")
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+    }
 }
