47b4d476bcc4f68abe9bf0b63c1cdd7f38d05586
김지하 <kimjiha1112@gmail.com>
2024-07-30 16:13:40 +0900

    introduction of @state

--- helloSwiftUI/ContentView.swift
+++ helloSwiftUI/ContentView.swift
@@ -7,30 +7,17 @@
 
 import SwiftUI
 
-struct Hike: Identifiable, Hashable {
-    let id = UUID()
-    let name: String
-    let photo: String
-    let miles: Double
-}
-
 struct ContentView: View {
     
-    let hikes = [
-        Hike(name: "Salmonberry Trail", photo: "sal", miles: 6),
-        Hike(name: "Tom, Dick, and Harry Mountain", photo: "tom", miles: 5.8),
-        Hike(name: "Tamanawas Falls", photo: "tam", miles: 5)
-    ]
+    @State private var count: Int = 0
     
     var body: some View {
-        NavigationStack {
-            List(hikes) { hike in
-                NavigationLink(value: hike) {
-                    HikeCellView(hike: hike)
-                }
-            }.navigationTitle("Hikes")
-            .navigationDestination(for: Hike.self) { hike in
-                HikeDetailScreen(hike: hike)
+        VStack {
+            Text("Hello World!")
+            Text("\(count)")
+                .font(.largeTitle)
+            Button("Increment") {
+                count += 1
             }
         }
     }
@@ -38,24 +25,4 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
-}
-
-struct HikeCellView: View {
-    
-    let hike: Hike
-    
-    var body: some View {
-        HStack(alignment: .top) {
-            Image(hike.photo)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .clipShape(RoundedRectangle(cornerRadius: 10.0, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
-                .frame(width: 100)
-            
-            VStack(alignment: .leading) {
-                Text(hike.name)
-                Text("\(hike.miles.formatted()) miles")
-            }
-        }
-    }
 }
