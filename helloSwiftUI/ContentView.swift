1a06e95883f20494bf8b3677474939587975a827
김지하 <kimjiha1112@gmail.com>
2024-07-30 16:26:14 +0900

    list and swiftUI

--- helloSwiftUI/ContentView.swift
+++ helloSwiftUI/ContentView.swift
@@ -9,14 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var isOn: Bool = false
+    @State private var name: String = ""
+    @State private var friends: [String] = []
     
     var body: some View {
         VStack {
-            Toggle(isOn: $isOn, label: {
-                Text(isOn ? "ON" : "OFF")
-            }).fixedSize()
-        }.frame(maxWidth: .infinity, maxHeight: .infinity).background(isOn ? .black : .red)
+            TextField("Enter name", text: $name)
+                .textFieldStyle(.roundedBorder)
+                .onSubmit {
+                    friends.append(name)
+                    name = ""
+                }
+            List(friends, id: \.self) { friend in
+                Text(friend)
+            }
+            Spacer()
+        }.padding()
     }
 }
 
