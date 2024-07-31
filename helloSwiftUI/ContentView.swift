a2aa58b111a0490a041ea36431485d398a2666c0
김지하 <kimjiha1112@gmail.com>
2024-07-31 22:57:50 +0900

    Refactor

--- helloSwiftUI/ContentView.swift
+++ helloSwiftUI/ContentView.swift
@@ -8,18 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var city: String = ""
+    @State private var isFetchingWeather: Bool = false
+    
+    let geocodingClient = GeocodingClient()
+    let weatherClient = WeatherClient()
+    
+    @State private var weather: Weather?
+    
+    private func fetchWeather() async {
+        do {
+            guard let location = try await geocodingClient.coordinateByCity(city) else { return }
+            weather = try await weatherClient.fetchWeather(location: location)
+        } catch {
+            print(error)
+        }
+    }
+    
     var body: some View {
         VStack {
-            Button("Get Coordinates") {
-                Task {
-                    let geocodingClient = GeocodingClient()
-                    let weatherClient = WeatherClient()
-                    
-                    let location = try! await geocodingClient.coordinateByCity("Houston")
-                    let weather = try! await weatherClient.fetchWeather(location: location!)
-                    print(weather)
+            TextField("City", text: $city)
+                .textFieldStyle(.roundedBorder)
+                .onSubmit {
+                    isFetchingWeather = true
+                }.task(id: isFetchingWeather) {
+                    if isFetchingWeather {
+                        await fetchWeather()
+                        isFetchingWeather = false
+                        city = ""
+                    }
                 }
+            
+            Spacer()
+            if let weather {
+                Text(MeasurementFormatter.temperature(value: weather.temp))
+                    .font(.system(size: 100))
             }
+            Spacer()
         }
         .padding()
     }
